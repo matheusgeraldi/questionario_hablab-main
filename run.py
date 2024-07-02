8c68afb3e6b7a397180c4966bb9457f7e57dd37c
matheus.geraldi <matheus.geraldi@posgrad.ufsc.br>
2024-07-02 15:43:21 -0300

    publicacao heroku

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from questionario.__init__ import create_app
+import os
 
 
 init_app = create_app()
 
 if __name__ == '__main__':
-    init_app.run(debug=True)
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
